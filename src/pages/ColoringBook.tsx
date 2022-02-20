3e848ee49abb580fb4cbd8679b0d8aeee25e2eb1
Dimitra Weinstein <drenee.weinstein@gmail.com>
2022-02-20 11:46:44 -0800

    add wip magician functionality

--- src/pages/ColoringBook.tsx
+++ src/pages/ColoringBook.tsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Magician from '../assets/Magician';
 import Toolbar from '../components/Toolbar';
+import { useRecordColor }from '../utils/useRecordColor';
 
 function ColoringBook(): JSX.Element {
   
+  const [selectedColor, setSelectedColor] = useState('#FFFFFF');
+  const [pathFill, setPathFill] = useState('#FFFFFF')
+  const { currentColor, undoColor, redoColor, recordColor } = useRecordColor();
+
+  const handleColorClick = () => {
+    setSelectedColor('#ff0000');
+    setPathFill(selectedColor);
+  }
+
+
   return (
     <div>
       <Toolbar />
       <div style={{display: 'flex'}}>
-        
+        <Magician
+          handleColorClick={handleColorClick}
+          pathColor={pathFill}
+        />
       </div>
     </div>
   )
